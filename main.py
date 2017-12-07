29fa2be4f3d908a9047001fe8f1e1c2f56591696
lookbehind <lookbehind@unknown.com>
2017-12-07 18:25:24 +0100

    Build a randomized match making.

--- main.py
+++ main.py
@@ -2,8 +2,34 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import random
 from my_config_parser import MyConfigParser
 from mailer import Mailer
+
+def match_mail_recipients(recipients):
+    new_list = {}
+    mixed_list = recipients.copy()
+    original_list = recipients.copy()
+    
+    while len(mixed_list) > 0:
+        if len(recipients) > 0:
+            recipient = random.choice(list(recipients.keys()))
+        else:
+            break
+        while True:
+            partner = random.choice(list(mixed_list.keys()))
+            if original_list[recipient] != original_list[partner]:
+                break
+            elif len(mixed_list) <= 1:
+                mixed_list = original_list.copy()
+                recipients = original_list.copy()
+            
+        new_list[recipient] = original_list[recipient]
+        new_list[recipient]['partner'] = original_list[partner]['name']
+        mixed_list.pop(partner)
+        recipients.pop(recipient)
+        
+    return new_list
 
 try:
     config = MyConfigParser("config/testconfig.yml")
@@ -15,8 +41,10 @@ except ValueError as val_err:
 mailer = Mailer(config['SMTP'])
 mailer.connect()
 maildata = config['Mail']
-for recipient in config['Recipients']:
-    maildata['to'] = config['Recipients'][recipient]['mail']
+partner_list = match_mail_recipients(config['Recipients'])
+for recipient in partner_list:
+    maildata['to'] = partner_list[recipient]['mail']
+    maildata['message'] = partner_list[recipient]['name'] + " matches " + partner_list[recipient]['partner']
     mailer.sendmail(maildata)
 mailer.disconnect()
 exit(0)
